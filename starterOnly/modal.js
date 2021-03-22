df7d7882299d51ab5cbd875899686b6ee52f9ec0
Math <mathilde.hetru@gmail.com>
2021-03-22 14:34:19 +0100

    form validations

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,8 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
-const thankBtn = document.querySelectorAll(".thank-btn");
-const thankBg = document.querySelector(".bground-thank");
+
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -22,18 +21,116 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
-// launch thank event
+/* launch thank event
 thankBtn.forEach((btn) => btn.addEventListener("click", launchThank));
-
-// launch thank form
 function launchThank() {
   thankBg.style.display = "block";
-}
+}*/
+
+document.getElementById("inscription").addEventListener("click", function(f) {
+
+	let erreur;
+	let firstName = document.getElementById("first");
+	let lastName = document.getElementById("last");
+	let emailValid = document.getElementById("email");
+	let birthValid = document.getElementById("birthdate");
+	let quantityCity = document.getElementById("quantity");
+	let checkBox = document.getElementById("checkbox1");
+
+	const thankBtn = document.querySelectorAll(".thank-btn");
+	const thankBg = document.querySelector(".bground-thank");
+
+	if (!firstName.value) {
+		firstName.oninvalid = function(e) {
+			e.target.setCustomValidity("");
+			if (!e.target.validity.valid) {
+				if (e.target.value.length == 0) {
+					f.preventDefault();
+					e.target.setCustomValidity("Veuillez entrer 2 caractères ou plus pour le champ du prénom.");
+				}
+			}
+		};		
+	}
+	if (!lastName.value) {
+		lastName.oninvalid = function(e) {
+			e.target.setCustomValidity("");
+			if (!e.target.validity.valid) {
+				if (e.target.value.length == 0) {
+					f.preventDefault();
+					e.target.setCustomValidity("Veuillez entrer 2 caractères ou plus pour le champ du nom.");
+				}
+			}
+		};
+	} 
+	if (!emailValid.value) {
+		emailValid.oninvalid = function(e) {
+			e.target.setCustomValidity("");
+			if (!e.target.validity.valid) {
+				if (e.target.value.length == 0) {
+					f.preventDefault();
+					e.target.setCustomValidity("Entrez une adresse valide. Exemple : contact@nom.com");
+				}
+			}
+		};
+	}
+	if (!birthValid.value) {
+		birthValid.oninvalid = function(e) {
+			e.target.setCustomValidity("");
+			if (!e.target.validity.valid) {
+				if (e.target.value.length == 0) {
+					f.preventDefault();
+					e.target.setCustomValidity("Vous devez entrer votre date de naissance.");
+				}
+			}
+		};
+	}
+	if (!quantityCity.value) {
+		quantityCity.oninvalid = function(e) {
+			e.target.setCustomValidity("");
+			if (!e.target.validity.valid) {
+				if (e.target.value.length == 0) {
+					f.preventDefault();
+					e.target.setCustomValidity("Vous devez renseigner ce champ.");
+				}
+			}
+		};
+	}
+	if (!checkBox.value) {
+		checkBox.oninvalid = function(e) {
+			e.target.setCustomValidity("");
+			if (!e.target.validity.valid) {
+				if (e.target.value.length == 0) {
+					f.preventDefault();
+					e.target.setCustomValidity("Vous devez accepter les conditions.");
+				}
+			}
+		};
+	}
+	else {
+		thankBtn.forEach((btn) => btn.addEventListener("click", launchThank));
+		function launchThank() {
+			thankBg.style.display = "block";
+		  }
+		}
+	}
+	);
 
 
-const SubForm = document.querySelector(".button");
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 // FORMULAIRE
 
 //validation du prénom
@@ -42,8 +139,6 @@ firstName.oninvalid = function(e) {
 	e.target.setCustomValidity("");
 	if (!e.target.validity.valid) {
 		if (e.target.value.length == 0) {
-e.target.setCustomValidity("Ce champ est obligatoire");
-		} else {
 e.target.setCustomValidity("Veuillez entrer 2 caractères ou plus pour le champ du prénom.");
 		}
 	}
@@ -87,3 +182,11 @@ e.target.setCustomValidity("Vous devez entrer votre date de naissance.");
 		}
 	}
 };
+
+// empecher le comportement par défaut = rechargement de la page
+const inscription = document.getElementById("inscription");
+inscription.addEventListener("click", function e(){
+	e.preventDefault();
+});*/
+
+
